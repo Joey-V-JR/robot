ab3a4420457de742741e5e8243d4a94a9fe04fa3
Joey-V-JR <nospam2153584992@gmail.com>
2024-03-30 17:58:15 -0400

    voltage and cylider movement fixes

--- main_code_robot.ino.ino
+++ main_code_robot.ino.ino
@@ -14,6 +14,7 @@
  */
 
 #include "MeMegaPi.h"
+#include "MeRGBLed.h"
 
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
@@ -21,6 +22,7 @@ MeEncoderOnBoard Encoder_2(SLOT2);
 MeUltrasonicSensor ultraSensor(PORT_8); 
 
 MePort port(PORT_5);
+MeRGBLed led(PORT_7);
 
 MeLineFollower lineFinder(PORT_6); 
 
@@ -29,12 +31,19 @@ MeMegaPiDCMotor motor2(PORT1B);
 MeMegaPiDCMotor motor3(PORT2A);
 MeMegaPiDCMotor motor4(PORT2B);
 
+int t, j, f, k;
+
 Servo myservo1;                                                // Create servo object to control a servo 
 int16_t servo1pin =  port.pin1();                              // Attaches the servo on PORT_5 SLOT1 to the servo object
 int left = 0;
 int right = 0;
 //int sensorState = lineFinder.readSensor();
 
+// ************************************************************************************
+
+int battV = 10.6;
+
+// *********************************************************************************8
 bool startTurn = true;
 int cylCount = 0;
 bool tourette = true;
@@ -43,6 +52,10 @@ bool loopDec = false;
 int counter1 = 0;
 int bumpC = -1;
 bool jerkSpeed = false;
+bool endMove = false;
+bool bumpCyl = false;
+int delayTimeTurn;
+int startTurnTime;
 
 void isr_process_encoder1(void)                               // This sets up the interrupt process for encoder 1
 {
@@ -93,6 +106,22 @@ void setup()
   Encoder_2.setPosPid(1.8,0,1.2);                       //Sets the PIDs for maintaining a given postion DO NOT CHANGE!
   Encoder_1.setSpeedPid(0.18,0,0);                      //Sets the PIDs for maintaining a given speed DO NOT CHANGE!
   Encoder_2.setSpeedPid(0.18,0,0);                      //Sets the PIDs for maintaining a given speed DO NOT CHANGE!!
+  
+      if (battV < 10)
+      {
+        delayTimeTurn = 260;
+        startTurnTime = 80;
+      }
+      else if (battV > 11)
+      {
+        delayTimeTurn = 220;
+        startTurnTime = 50;
+      }
+      else
+      {
+        delayTimeTurn = 240;
+        startTurnTime = 70;
+      }
 } 
 
 void loop() 
@@ -104,9 +133,21 @@ void loop()
       headN();
       delay (150);
   }
-  if (jerkSpeed == true)
+  if (startTurn == true)
+  {
+    moveFmid ();
+
+  }
+  else if (jerkSpeed == true)
   {
     moveFsli ();
+  }
+  else if (cylCount >= 3 and endMove == false)
+  {
+    moveFultra();
+    turnL();
+    moveFultra();
+    endMove = true;
   }
   else
   {
@@ -133,7 +174,15 @@ void loop()
           break;
           
     case S1_OUT_S2_OUT: Serial.println("Sensor 1 and 2 are outside of black line"); 
-          backupFast();
+                delay(50);
+                for(uint8_t t = 0; t <= bumpC + 1; t++)
+                {
+                  led.setColorAt(t, 254, 0, 0);
+                }
+                led.show();
+                //led.reset();
+                delay(50);
+                backupFast();
           
           bumpC = bumpC + 1;
           
@@ -148,9 +197,15 @@ void loop()
           {
             if (cylCount == 1 or cylCount == 3)
             {
-              turnL();
               turnLsli();
               delay(100);
+              //turnL();
+              turnLsli();
+              delay(100);
+              turnLsli();
+              delay(100);
+              //turnLsli();
+              //delay(100);
               bumpCreset ();
               
               
@@ -160,6 +215,7 @@ void loop()
             {
               turnR();
               delay(100);
+              bumpCyl = true;
               bumpCreset ();
              
             }
@@ -173,14 +229,21 @@ void loop()
           break;
   }
   delay(50);
-      while( startTurn == false and servoAng < 160 and counter1 > 12 and cylCount < 3)
+      while( startTurn == false and servoAng < 165 and counter1 > 12 and cylCount < 3)
       {
               
               tourette = false;
               myservo1.write(servoAng);
-              delay (150);
+              delay (100); // down from 150
+              if (servoAng < 20)
+              {
+                delay(50);
+              }
+              else
+              {
+                ultraSonic ();
+              }
               servoAng = servoAng + 5;
-              ultraSonic ();
               delay (150);
               loopDec = true;      
       }   
@@ -202,7 +265,7 @@ void loop()
       
   
   
-}
+  }
 
  void backupFast () 
   {
@@ -210,19 +273,19 @@ void loop()
           motor2.stop();
           motor3.stop();
           motor4.stop();
-          delay(500);
+          delay(300);
           if (startTurn == true)
           {
             motor1.run(250);
             motor2.run(250);
             motor3.run(-250);
             motor4.run(-250);
-            delay(80); 
+            delay(startTurnTime); 
             motor1.stop();
             motor2.stop();
             motor3.stop();
             motor4.stop();
-            delay (80);
+            delay (startTurnTime);
           }
 
           else
@@ -255,12 +318,12 @@ void loop()
       motor2.run(250);
       motor3.run(250);
       motor4.run(250);
-      delay(260);
+      delay(delayTimeTurn);
       motor1.stop();
       motor2.stop(); 
       motor3.stop();
       motor4.stop(); 
-      delay (260);
+      delay (delayTimeTurn);
   }
 
   void turnLsli()
@@ -269,28 +332,29 @@ void loop()
       motor2.run(250);
       motor3.run(250);
       motor4.run(250);
-      delay(85);
+      delay(startTurnTime);
       motor1.stop();
       motor2.stop();
       motor3.stop();
       motor4.stop(); 
-      delay (100);
+      delay (startTurnTime);
   }
 
   void turnR()
   {
     //turn issues
        //backupFast();
+      
       motor1.run(-250);
       motor2.run(-250);
       motor3.run(-250);
       motor4.run(-250);
-      delay(260);
+      delay(delayTimeTurn);
       motor1.stop();
       motor2.stop();
       motor3.stop();
       motor4.stop(); 
-      delay (260);
+      delay (delayTimeTurn);
   }
 
   void turnRsli()
@@ -299,12 +363,12 @@ void loop()
       motor2.run(-250);
       motor3.run(-250);
       motor4.run(-250);
-      delay(85);
+      delay(startTurnTime);
       motor1.stop();
       motor2.stop();
       motor3.stop();
       motor4.stop(); 
-      delay (100);
+      delay (startTurnTime);
   }
 
   void turnCyl ()
@@ -379,6 +443,19 @@ void loop()
       delay(40);
     
   }
+  void moveFultra ()
+  {
+      motor1.run(-250);
+      motor2.run(-250);
+      motor3.run(250);
+      motor4.run(250);
+      delay(300); 
+      motor1.stop();
+      motor2.stop();
+      motor3.stop();
+      motor4.stop();
+
+  }
   void moveF ()
   {
       motor1.run(-80);
@@ -393,9 +470,23 @@ void loop()
       //delay(400);
   }
 
+  void moveFmid ()
+  {
+     motor1.run(-70);
+      motor2.run(-70);
+      motor3.run(70);
+      motor4.run(70);
+      delay(350); 
+      motor1.stop();
+      motor2.stop();
+      motor3.stop();
+      motor4.stop();
+      //delay(400);
+  }
+
   void moveFsli ()
   {
-    motor1.run(-45);
+      motor1.run(-45);
       motor2.run(-45);
       motor3.run(45);
       motor4.run(45);
@@ -410,25 +501,25 @@ void loop()
   {
       if(ultraSensor.distanceCm() <= 15)
         {
-               motor1.stop();
-               motor2.stop();
-               motor3.stop();
-               motor4.stop();
-               delay(2000);
+              motor1.stop();
+              motor2.stop();
+              motor3.stop();
+              motor4.stop();
+              LEDcylDis(254,254,254);
               
               Encoder_1.reset(SLOT1);
               Encoder_2.reset(SLOT2);
                
               myservo1.write(65);
-              delay(1000);             
+              delay(500);             
               left = ultraSensor.distanceCm();
-              delay(1000);
-          
+              delay(500);
+              
               myservo1.write(115);
-              delay(1000);             
+              delay(500);             
               right = ultraSensor.distanceCm();
-              delay(1000);
-          
+              delay(500);
+              
               myservo1.write(90);
               delay(200);        
               //do
@@ -439,6 +530,7 @@ void loop()
                   //Encoder_1.moveTo(200,50);
                   //Encoder_2.moveTo(200,50);
                   turnL();
+                  turnRsli();
                   cylCount++;
                   //loopDec = false;
                   counter1 = 0;
@@ -464,6 +556,7 @@ void loop()
                   {
                     cylCount++;
                     turnL();
+                    turnRsli();
                   }
                 }
                 if(left == right)
@@ -482,7 +575,9 @@ void loop()
             {
                 turnRsli();
                 moveFsli ();
-                servoAng = 20;
+                servoAng = 15;
+                // insert code
+                LEDcylDis(0, 254, 0);
                 
             }
 
@@ -490,7 +585,25 @@ void loop()
             {
                 turnLsli();
                 moveFsli ();
+                servoAng = 15;
+                // insert code
+                LEDcylDis(0, 254, 0);
+            }
+
+            else if (ultraSensor.distanceCm() <= 33 and servoAng < 70)
+            {
+                turnRsli();
+                moveF ();
                 servoAng = 20;
+                LEDcylDis(0, 0, 254);
+            }
+
+            else if (ultraSensor.distanceCm() <= 33 and servoAng > 110)
+            {
+                turnLsli();
+                moveF ();
+                servoAng = 20;
+                LEDcylDis(0, 0, 254);
             }
 
             
@@ -503,12 +616,47 @@ void loop()
 
         jerkSpeed = false;
         bumpC = 0;
-        if (cylCount == 2)
+        led.reset(PORT_7);
+        if (cylCount == 2 and bumpCyl == true)
         {
-          counter1 = 1;
+          counter1 = 5;
+          bumpC = false;
         }
         else
         {
           counter1 = 0;
         }
   }
+
+void color_loop ()
+    {
+    for(uint8_t t = 0; t < 3; t++)
+    {
+      uint8_t red	= 64 * (1 + sin(t / 2.0 + j / 4.0) );
+      uint8_t green = 64 * (1 + sin(t / 1.0 + f / 9.0 + 2.1) );
+      uint8_t blue = 64 * (1 + sin(t / 3.0 + k / 14.0 + 4.2) );
+      led.setColorAt(t, red, green, blue);
+    }
+    led.show();
+    j += random(1, 6) / 6.0;
+    f += random(1, 6) / 6.0;
+    k += random(1, 6) / 6.0;
+  }  
+
+  void LEDcylDis(int R, int G, int B)
+
+  {
+                delay(400);
+                led.reset(PORT_7);
+                delay(50);
+                for(t = 0; t <= cylCount; t++)
+                  {
+                    uint8_t red	= 64 * (1 + sin(t / 2.0 + j / 4.0) );
+                    uint8_t green = 64 * (1 + sin(t / 1.0 + f / 9.0 + 2.1) );
+                    uint8_t blue = 64 * (1 + sin(t / 3.0 + k / 14.0 + 4.2) );
+                    led.setColorAt(t, R, G, B);
+                  }
+                  led.show();
+                  delay(400);
+
+  }  
